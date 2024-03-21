9614dd77b07a99cc4165eb7719d1187cd1e42849
lamiaaibrahim <lamiaelhooby@gmail.com>
2024-03-21 08:03:48 +0200

    Update meals page with metadata, revalidate meals page on meal save, and update meals database and image
    * Add `metadata` to meals page with title "All Meals" and description "Browse our delicious meals"
    * Revalidate meals page on meal save in `shareMeal` action
    * Update meals database with new meal in `shareMeal` action
    * Replace image `s.png` in `public/images` directory

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import load from "./loading.module.css";
 import MealsGrid from "../components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse our delicious meals",
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
